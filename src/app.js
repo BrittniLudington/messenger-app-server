3c96de9d52daf47d92412dc45962bd0cae1704a8
BrittniLudington <brittniludington@gmail.com>
2019-07-17 14:39:23 -0700

    attempting to fix cors problem

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(cors());
 app.options('*',cors());
+
+app.use(function(req,res,next)
+{
+    res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.use(helmet());
 
 app.use(parser.json());
