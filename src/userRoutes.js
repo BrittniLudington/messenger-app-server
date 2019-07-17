abeeb7219b8420e7d86a7a6d470a85f65d09addd
BrittniLudington <brittniludington@gmail.com>
2019-07-17 14:28:30 -0700

    attempting to fix cors problem

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -6,6 +6,13 @@ let userRoutes = express.Router();
 
 //userRoutes.all(requireAuth);
 
+userRoutes.use(function(req,res,next)
+{
+    res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 userRoutes.get('/users',(req,resApp)=>
 {
     client.query(`SELECT * from users;`,(err,res)=>
